902d507013d65beb638145cec162e3b086edc56a
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2024-08-22 22:45:53 +0330

    Fix build error

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { FormattedMessage } from "react-intl";
-
 export default function Loading() {
-	return (
-		<>
-			<FormattedMessage defaultMessage={"Loading Text"} id="loading.text" />
-		</>
-	);
+	return <>Loading...</>;
 }
